60d9ad8b479b1bbb4ba9940a4af3e3d6164e3691
dcorrigan <dcorrigan@scribenet.com>
2014-07-30 15:11:23 -0400

    moved file_struct

---
+++ lib/file_struct.rb
@@ -0,0 +1,65 @@
+class FileStruct
+  attr_accessor :basename, :content, :path, :doc
+  attr_reader :ext
+
+  def initialize(path, options = {})
+    @path = path
+    @basename = File.basename(path)
+    @ext = File.extname(path)
+    @content = File.read(@path)
+  end
+
+  def basic_name
+    File.basename(@path, @ext)
+  end
+
+  def content
+    @content ||= File.read(@path)
+  end
+
+  def reload!
+    @content = File.read(@path)
+  end
+
+  def save!
+  end
+end
+
+class XMLFileStruct < FileStruct
+  def doc
+    doc ||= Nokogiri.XML(@content)
+  end
+
+  def reload!
+    super
+    @doc = Nokogiri.XML(@content)
+  end
+
+  def save!
+    @content = @doc.to_xml(encoding: 'UTF-8', indent: 2)
+  end
+
+  def content
+    @content ||= @doc.to_xml(encoding: 'UTF-8', indent: 2)
+  end
+end
+
+class YAMLFileStruct < FileStruct
+  def doc
+    @doc ||= YAML.load(@content)
+  end
+
+  def reload!
+    super
+    @doc = YAML.load(@content)
+  end
+
+  def save!
+    @content = @doc.to_yaml
+  end
+
+  def content
+    @content ||= @doc.to_yaml
+  end
+end
+
